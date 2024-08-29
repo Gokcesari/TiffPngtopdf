9ee826cd94c27a77e22c812b1793200eee0c8c9e
Gökçç <144737017+Gokcesari@users.noreply.github.com>
2024-08-29 16:27:38 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Tiff2PDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.lstbx = new System.Windows.Forms.ListBox();
+            this.tbxFolderPath = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtb
+            // 
+            this.rtb.Location = new System.Drawing.Point(12, 166);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(755, 279);
+            this.rtb.TabIndex = 0;
+            this.rtb.Text = "";
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(618, 101);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(75, 23);
+            this.btn.TabIndex = 2;
+            this.btn.Text = "Dosya seç";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstbx
+            // 
+            this.lstbx.FormattingEnabled = true;
+            this.lstbx.Location = new System.Drawing.Point(295, 94);
+            this.lstbx.Name = "lstbx";
+            this.lstbx.Size = new System.Drawing.Size(200, 56);
+            this.lstbx.TabIndex = 3;
+            // 
+            // tbxFolderPath
+            // 
+            this.tbxFolderPath.Location = new System.Drawing.Point(607, 130);
+            this.tbxFolderPath.Name = "tbxFolderPath";
+            this.tbxFolderPath.Size = new System.Drawing.Size(100, 20);
+            this.tbxFolderPath.TabIndex = 4;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(365, 47);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 41);
+            this.button.TabIndex = 5;
+            this.button.Text = "Tiff\'i pdfe Çevir";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.tbxFolderPath);
+            this.Controls.Add(this.lstbx);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.rtb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.ListBox lstbx;
+        private System.Windows.Forms.TextBox tbxFolderPath;
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Drawing.Printing;
+using System.Xml.Linq;
+using System.Drawing.Drawing2D;
+using Tesseract;
+using iText.Layout.Renderer;
+using iText.Layout.Element;
+
+namespace Tiff2PDF
+{
+    public partial class Form1 : Form
+    {
+
+        public String tiffDir = "";
+        string[] tiffFiles;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void chooseTiffDir()
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                tiffDir = fbd.SelectedPath;
+
+                tiffFiles = Directory.GetFiles(@tiffDir, "*.tif");
+                if (tiffFiles.Length == 0)
+                {
+                    rtb.AppendText("\nNo Tiff Files in" + tiffDir);
+                    DialogResult dr = MessageBox.Show("Would you like to select another directory?", "No Tiff Files Found!", MessageBoxButtons.YesNo);
+                    switch (dr)
+                    {
+                        case DialogResult.Yes:
+                            chooseTiffDir();
+                            break;
+                        case DialogResult.No:
+                            return;
+                    }
+                }
+                else
+                {   // rtb is the RichTextBox
+                    //ltbx is the ListBox
+                    //textbx is the TextBox
+
+                    rtb.AppendText("\n" + tiffFiles.Length + " Tiff File(s) Found");
+                    tbxFolderPath.Text = fbd.SelectedPath;
+                    for (int i = 0; i < tiffFiles.Length; i++)
+                    {
+                        lstbx.Items.Add(tiffFiles[i].Substring(tbxFolderPath.Text.Length + 1));
+                        rtb.AppendText("\n" + tiffFiles[i].Substring(tbxFolderPath.Text.Length + 1) + " found!");
+                    }
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rtb.AppendText("Launch: Successful");
+
+        }
+
+        private void btn_tiffDir_Click(object sender, EventArgs e)
+        {
+            chooseTiffDir();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chooseTiffDir();
+
+
+        }
+
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            string folderPath = tbxFolderPath.Text;
+            var files = Directory.GetFiles(folderPath, ".");
+
+            if (files == null)
+            {
+                MessageBox.Show("Dosya bulunamadı");
+                return;
+            }
+
+
+            foreach (var file in files)
+            {
+                SearchablePdfCreate(file, Path.ChangeExtension(file, "pdf"), "tur");
+
+                rtb.AppendText(file + " PDF oluşturuldu");
+            }
+
+        }
+       
+
+            public void SearchablePdfCreate(string inputName, string outputName, string dil)
+            {
+                int sayfaSayisi = 0;
+                int toplamSayfa = 0;
+                using (IResultRenderer renderer = Tesseract.PdfResultRenderer.CreatePdfRenderer(outputName, @"./tessdata", false))
+                {
+                    // PDF Title
+                    using (renderer.BeginDocument(outputName))
+                    {
+                        using (TesseractEngine engine = new TesseractEngine(AppContext.BaseDirectory + "tessdata\\", dil, EngineMode.Default))
+                        {
+                            using (PixArray pages = PixArray.LoadMultiPageTiffFromFile(inputName))
+                            {
+                                toplamSayfa = pages.Count;
+                                foreach (Pix p in pages)
+                                {
+                                    Application.DoEvents();
+                                    using (var page = engine.Process(p, outputName))
+                                    {
+                                        renderer.AddPage(page);
+                                        sayfaSayisi++;
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+
+
+
+        }
+    }
+
+
+
